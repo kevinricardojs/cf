106f749506230cfc09948670815d5cea2c4fecf4
kevinricardojs <kevinricardojs@gmail.com>
2015-09-14 01:06:11 -0600

    Toque final

--- app/models/comentario.rb
+++ app/models/comentario.rb
@@ -1,7 +1,7 @@
 class Comentario < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :correo, format: { with: VALID_EMAIL_REGEX,message: "no valido" }
-	validates_presence_of :correo, message: "puede estar vacio"
+	validates_presence_of :correo, message: "no puede estar vacio"
 	validates :mensaje, length: {minimum: 10, message: "tiene que tener un minimo de carateres de 10"}
 	validates_presence_of :mensaje, message: "no puede estar vacio"
 end
